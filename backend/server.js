734ec4efa343edbdea49d92dcf3e7325ee3ba8d9
KARAN-SINGH19 <115944579+KARAN-SINGH19@users.noreply.github.com>
2024-08-17 12:19:46 +0400

    route added

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,6 @@ dotenv.config({ path: '../backend/config.env' })
 //CONNECTING DATABASE
 connect()
 
-router.get('/', (req, res) => {
-    res.send('Welcome to the Talent Vault App Backend!');
-});
-
 //MAKING SERVER WHICH WILL RECEIVE ALL KINDS OF REQUESTS
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
